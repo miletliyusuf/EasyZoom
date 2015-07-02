c6a7448602ecbee1339ced9c642f2cc0cac2b183
miletliyusuf <miletliyusuf@gmail.com>
2015-07-02 17:48:30 +0300

    Autolayout will be fixed

--- Pod/Classes/EasyZoom.swift
+++ Pod/Classes/EasyZoom.swift
@@ -9,6 +9,9 @@ public class EasyZoom: NSObject,UIScrollViewDelegate {
 	//Instant imageView and ScrollView which will show your image and will make them zoom.
 	var instantImageView =  UIImageView()
 	var instantScrollView = UIScrollView()
+	var instantView = UIView()
+	
+	var didDoubleTapped = Bool()
 	
 	/**
 		This public function adds zoom for imageViews.
@@ -20,14 +23,31 @@ public class EasyZoom: NSObject,UIScrollViewDelegate {
 		
 		//delegating of instant scrollView
 		instantScrollView.delegate = self
+		
+		instantView.frame = superView.frame
+		superView.addSubview(instantView)
+		
 		//setting instant scrollView frame to user imageView
-		instantScrollView.frame = superView.frame
-		imageView.removeFromSuperview()
+		instantScrollView.frame = instantView.frame
+		
+//		instantView.setTranslatesAutoresizingMaskIntoConstraints(false)
+		let viewDictionary = ["imageview":instantImageView,"uiview":instantView]
+//		let instantViewVCons:Array = NSLayoutConstraint.constraintsWithVisualFormat("H:|-[uiview]-|", options: NSLayoutFormatOptions(0), metrics: nil, views:viewDictionary)
+//		let instantViewHCons:Array = NSLayoutConstraint.constraintsWithVisualFormat("V:|-[uiview]-|", options: NSLayoutFormatOptions(0), metrics: nil, views: viewDictionary)
+//		
+//		superView.addConstraints(instantViewHCons)
+//		superView.addConstraints(instantViewVCons)
+		
+//		imageView.removeFromSuperview()
 		instantImageView.image = image
-		instantImageView.frame = CGRect(origin: CGPoint(x: 0, y: 0), size: image.size)
+//		instantImageView.frame = CGRect(origin: CGPoint(x: 0, y: 0), size: image.size)
 		instantScrollView.addSubview(instantImageView)
 		
-		superView.addSubview(instantScrollView)
+		instantImageView.setTranslatesAutoresizingMaskIntoConstraints(false)
+		let horizontalConstraints:Array = NSLayoutConstraint.constraintsWithVisualFormat("H:|-[imageview]-|", options: NSLayoutFormatOptions(0), metrics: nil, views:viewDictionary)
+		let verticalConstraints:Array = NSLayoutConstraint.constraintsWithVisualFormat("V:|-[imageview]-|", options: NSLayoutFormatOptions(0), metrics: nil, views: viewDictionary)
+		
+		instantView.addSubview(instantScrollView)
 		
 		instantScrollView.contentSize = image.size
 		
@@ -36,7 +56,10 @@ public class EasyZoom: NSObject,UIScrollViewDelegate {
 		doubleTapRecognizer.numberOfTapsRequired = 2
 		doubleTapRecognizer.numberOfTouchesRequired = 1
 		instantScrollView.addGestureRecognizer(doubleTapRecognizer)
+		didDoubleTapped = true
 		
+		var panGesture = UIPanGestureRecognizer(target: self, action: "handlePan:")
+		instantScrollView.addGestureRecognizer(panGesture)
 		
 		//setting minumum and maximum zoom scale
 		let scrollViewFrame = instantScrollView.frame
@@ -48,11 +71,34 @@ public class EasyZoom: NSObject,UIScrollViewDelegate {
 		instantScrollView.maximumZoomScale = 1.0
 		instantScrollView.zoomScale = minScale
 	
+		instantView.addConstraints(horizontalConstraints)
+		instantView.addConstraints(verticalConstraints)
+		
 		//centerizing content
-		centerizeContent(instantScrollView, imageView: instantImageView)
+//		centerizeContent(instantScrollView, imageView: instantImageView)
 	}
+	
+	public func handlePan(recognizer:UIPanGestureRecognizer) {
+		
+		UIApplication.sharedApplication().statusBarHidden = true
+		
+		let translation = recognizer.translationInView(instantView)
+		if let view = recognizer.view {
+			view.center = CGPoint(x: view.center.x + translation.x , y: view.center.y + translation.y)
+			if (view.center.y + instantImageView.bounds.midY > instantView.bounds.midY) {
+				instantView.backgroundColor = UIColor.whiteColor().colorWithAlphaComponent(0.3)
+				instantScrollView.zoomScale = 0
+				
+				centerizeContent(instantScrollView, imageView: instantImageView)
+//				instantView.removeFromSuperview()
+			}
+		}
+		
+		recognizer.setTranslation(CGPointZero, inView: instantView)
+	}
+	
 	/**
-	 Centerizing content in this instance method.
+	 Centerizing content.
 	 @param scrollView - UIScrollView
 	 @param imageView - UIImageView
 	 */
@@ -77,30 +123,38 @@ public class EasyZoom: NSObject,UIScrollViewDelegate {
 	}
 	
 	/**
-	 NSNotification handler method.
+	 NSNotification handler method. This methods recognize user tap area and zoom over there.
 	 @param recognizer - UITapGestureRecognizer
 	 */
 	func didDoubleTapped (recognizer:UITapGestureRecognizer) {
-
-		let pointInView = recognizer.locationInView(instantImageView)
 		
-		var newZoomScale = instantScrollView.zoomScale * 1.5
-		newZoomScale = min(newZoomScale, instantScrollView.maximumZoomScale)
-		
-		let scrollViewSize = instantScrollView.bounds.size
-		let w = scrollViewSize.width / newZoomScale
-		let h = scrollViewSize.height / newZoomScale
-		let x = pointInView.x - (w / 2.0)
-		let y = pointInView.y - (h / 2.0)
-		
-		let rectToZoomTo = CGRectMake(x,y, w, h)
-		
-		instantScrollView.zoomToRect(rectToZoomTo, animated: true)
+		if (didDoubleTapped == true) {
+			let pointInView = recognizer.locationInView(instantImageView)
+			
+			var newZoomScale = instantScrollView.zoomScale * 3.5
+			newZoomScale = min(newZoomScale, instantScrollView.maximumZoomScale)
+			
+			let scrollViewSize = instantScrollView.bounds.size
+			let w = scrollViewSize.width / newZoomScale
+			let h = scrollViewSize.height / newZoomScale
+			let x = pointInView.x - (w / 2.0)
+			let y = pointInView.y - (h / 2.0)
+			
+			let rectToZoomTo = CGRectMake(x,y, w, h)
+			
+			instantScrollView.zoomToRect(rectToZoomTo, animated: true)
+			didDoubleTapped = false
+		}
+		else {
+			instantScrollView.zoomScale = 0;
+			didDoubleTapped = true
+		}
 	}
 	
 	//pragma mark UIScrollView Delegate Methods
 	public func scrollViewDidZoom(scrollView: UIScrollView) {
-		centerizeContent(instantScrollView, imageView: instantImageView)
+//		centerizeContent(instantScrollView, imageView: instantImageView)
+//		didDoubleTapped = false
 	}
 	
 	public func viewForZoomingInScrollView(scrollView: UIScrollView) -> UIView? {
